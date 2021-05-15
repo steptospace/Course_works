ad5cabe732fd24a7207ca17bc1c55d88c3116623
Daniil <d.sawkow2014@gmail.com>
2021-05-15 12:43:51 +0300

    New start on this way

--- Crypto.py
+++ Crypto.py
@@ -4,13 +4,7 @@ from Cryptodome.Cipher import PKCS1_OAEP
 
 # Напишем функцию для генерации ключей
 def generate_keys(bits=2048):
-    """Generates the pair of private and public keys.
-
-    :param bits: <int> Key length, or size (in bits) 
-    of the RSA modulus (default 2048)
-    :return: <object> private_key, <object> public_key
-
-    """
+    """Generates the pair of private and public keys."""
     private_key = RSA.generate(bits)
     public_key = private_key.publickey()
     return private_key, public_key
@@ -26,17 +20,10 @@ print(public_key.exportKey(format='PEM').decode())
 '''
 message = b'Hello world!'
 messages = "Hello fkng world "
-print(message)
+
 
 def encrypt_message(message, public_key):
-    """Encripts the message using public_key.
-
-    :param message: <str> Message for encryption
-    :param public_key: <object> public_key
-    :param verbose: <bool> Print description;
-    :return: <object> Message encrypted with public_key
-
-    """
+    """Encripts the message using public_key."""
     cipher = PKCS1_OAEP.new(public_key)
     encrypted_message = cipher.encrypt(message)
     print(f'Message: {message} was encrypted to\n{encrypted_message.hex()}')
@@ -44,19 +31,11 @@ def encrypt_message(message, public_key):
 
 
 def decrypt_message(encrypted_message, private_key):
-    """Decripts the message using private_key and check it's hash
-
-    :param encrypted_message: <object> Encrypted message
-    :param private_key: <object> private_key
-    :return: <object> Message decripted with private_key
-
-    """
-
     cipher = PKCS1_OAEP.new(private_key)
     decrypted_message = cipher.decrypt(encrypted_message)
     print(decrypted_message)
 
-def encryp_hash (messages): # check this momment and be careful messages only STRING !!!
+def encryp_hash (messages): # check this moment and be careful messages only STRING !!!
     data_hash = SHA256.new(messages.encode())
     return data_hash
 
